8e2bf5d678a8f3dda6a8c7ac3061aa576b151f02
Vasu-Akula <vasubtech502@gmail.com>
2021-02-12 01:02:20 +0530

    commit

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
 
     def fn_uuid():
-        uid = uuid.uuid1()
+        uid = uuid.uuid4()
         return str(uid)
 
     fn_uuid = spark.udf.register("fn_uuid", fn_uuid, StringType())
