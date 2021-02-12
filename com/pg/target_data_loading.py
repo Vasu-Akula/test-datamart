d1d8e67fcf6631e9b6de9ee29830c2dc05aff125
Vasu-Akula <vasubtech502@gmail.com>
2021-02-12 19:03:11 +0530

    commit

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -8,6 +8,11 @@ import utils.aws_utils as ut
 
 if __name__ == '__main__':
 
+    os.environ["PYSPARK_SUBMIT_ARGS"] = (
+        '--jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar"\
+         --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" pyspark-shell'
+    )
+
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
     app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
@@ -30,6 +35,7 @@ if __name__ == '__main__':
         uid = uuid.uuid4()
         return str(uid)
 
+
     fn_uuid = spark.udf.register("fn_uuid", fn_uuid, StringType())
     tgt_list = app_conf['target_list']
 
@@ -40,15 +46,18 @@ if __name__ == '__main__':
             cp_df = spark.read.parquet(stg_loc + "/" + tgt_conf["source_data"])
             cp_df.printSchema()
             cp_df.createOrReplaceTempView(tgt_conf["source_data"])
-            cp_df.show()
+
             regis_dim_df = spark.sql(tgt_conf["loading_query"])
             regis_dim_df.show(5, False)
+            ut.write_into_redshift(regis_dim_df, app_secret, app_conf,"PUBLIC.REGIS_DIM")
         elif tgt == 'CHILD_DIM':
             cp_df = spark.read.parquet(stg_loc + "/" + tgt_conf["source_data"])
             cp_df.printSchema()
             cp_df.createOrReplaceTempView(tgt_conf["source_data"])
+
             child_dim_df = spark.sql(tgt_conf["loading_query"])
             child_dim_df.show(5, False)
+            ut.write_into_redshift(child_dim_df, app_secret, app_conf, "PUBLIC.CHILD_DIM")
 
 
 
--- utils/aws_utils.py
+++ utils/aws_utils.py
@@ -13,3 +13,14 @@ def get_mysql_jdbc_url(mysql_config: dict):
     port = mysql_config["mysql_conf"]["port"]
     database = mysql_config["mysql_conf"]["database"]
     return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host, port, database)
+
+def write_into_redshift(dim_df, app_secret, app_conf, tablename):
+    jdbc_url = get_redshift_jdbc_url(app_secret)
+    dim_df.coalesce(1).write \
+        .format("io.github.spark_redshift_community.spark.redshift") \
+        .option("url", jdbc_url) \
+        .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
+        .option("forward_spark_s3_credentials", "true") \
+        .option("dbtable", tablename) \
+        .mode("overwrite") \
+        .save()
